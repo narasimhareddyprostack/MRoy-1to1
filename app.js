adba482af13fbffcbff166b407f97d0027f103b3
Krishna Reddy <pkrishna.cap@outlook.com>
2020-09-01 13:01:04 +0530

    2nd class

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Hello Good Morning")
+
+//obj
+var emp = {
+    emp_no : 10,
+    "emp_Name" : "M Roy",
+    "Salary" : "2000 USD"
+
+}
+
+console.log(emp)
+
+
+
+var number_Name = ["one", "two"]
+
+console.log(number_Name)
+
+const wish = (name) => {
+    console.log("Hello", ` ${name}` )
+
+}
+wish('manasi')
+wish('narasimha')
+
+
