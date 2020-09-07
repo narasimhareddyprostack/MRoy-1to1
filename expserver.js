a8ee14f49d5a4d6dfe95a0ed136474b1d2713a55
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-09-07 13:56:28 +0530

    Q and A

--- expserver.js
+++ expserver.js
@@ -1,30 +1,19 @@
-const express = require('express')
-const app = express()
-app.get('/',(req,res) =>{
-    res.send("<h1> Welcome to PSA</h1>")
-})
+const express = require("express");
+const app = express();
 
-app.get('/about', (req,res) =>{
-    res.send("<h1>About Page</h1>");
-})
+app.get("/", (req, res) => {
+  res.send("<h1> Welcome to PSA</h1>");
+});
 
-app.get('/services', (req,res) =>{
-    res.send("<h1>Services Page</h1>");
-})
+app.get("/order", (req, res) => {
+  res.send(req.query);
+  //res.send(req.query.name);
+});
 
+app.get("/order/:id", (req, res) => {
+  res.send(req.params.id);
+});
 
-app.get('/order/', (req,res) =>{
-    const id = req.params.id;
-    res.send("<h1>Your Product</h1>"+id);
-})
-
-app.get('/order/:id',(req,res)=>{
-    const id = req.params.id;
-    res.send("Your Order ID:"+id)
-})
-
-app.listen(6500,(err)=>{
-    console.log("Express Server Running...");
-})
-
-
+app.listen(6500, (err) => {
+  console.log("Express Server Running...");
+});
