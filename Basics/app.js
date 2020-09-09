8110f1f1ca47b1db6c9faa6d0a44de01dfe2f77a
narasimhareddyprostack <narasimhareddyk18@gmail.comm>
2020-09-09 08:18:58 +0530

    streemline

---
+++ Basics/app.js
@@ -0,0 +1,22 @@
+//var express = require("express");
+
+import express from 'express'
+
+var app = express();
+
+app.get('/', (req,res)=>{
+ res.send("Hello - Welcome to PSA");
+});
+
+
+app.listen(8900,(err)=>{
+    console.log("Server Running....")
+});
+
+/*
+var express = require("express");
+var app = express();
+app.get();
+app.listen(8900);
+*/
+/*C R U D */
