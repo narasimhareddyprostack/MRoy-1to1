978b28a8a4a66ae876c10e16a9cf74d1e847b886
narasimhareddyprostack <narasimhareddyk18@gmail.comm>
2020-09-09 09:08:33 +0530

    es6

---
+++ SeedApp - Copy/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+//es6
+var app = express();
+app.get("/", (req, res) => {
+  res.send("Hello - Node with ES6- successfully");
+});
+app.listen(5000, (err) => {
+  console.log(`Server Running on Port: ${5000}`);
+});
