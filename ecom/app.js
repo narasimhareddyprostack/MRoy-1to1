43141613a6d09b0244b6f4d5224528ee901a179f
narasimhareddyprostack <narasimhareddyk18@gmail.comm>
2020-09-10 09:32:23 +0530

    ejs

--- ecom/app.js
+++ ecom/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 //es6
 var app = express();
+//static path setting
+app.use(express.static(__dirname + "/public"));
+app.set("views", "./src/view");
+
+app.set("view engine", "ejs");
+var message = 'BookMyShow';
 app.get("/", (req, res) => {
-  res.send("<h1>Ecom Project</h1>");
+  res.render("index",{ message });
 });
 app.listen(8000, (err) => {
   console.log(`Server Running on Port: ${8000}`);
